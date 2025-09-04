d6eea3602fb072ca02def79ba2f8c4fa930c9ad4
Corey Barbera <160194877+Dismodified@users.noreply.github.com>
2025-09-04 12:53:48 -0400

    Update SearchContacts.php

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -5,7 +5,7 @@
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	$conn = new mysqli("localhost", "Great", "White", "SharkWeek");
 if ($conn->connect_error) 
     {
         returnWithError( $conn->connect_error );
@@ -79,3 +79,4 @@ if ($conn->connect_error)
     }
     
 ?>
+
