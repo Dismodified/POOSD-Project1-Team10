ce35d62df6c7b49ef49c1a14eb224bf2724af7ad
Corey Barbera <160194877+Dismodified@users.noreply.github.com>
2025-09-04 12:52:30 -0400

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -6,7 +6,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	$conn = new mysqli("localhost", "Great", "White", "SharkWeek"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
@@ -55,3 +55,4 @@
 	}
 	
 ?>
+
