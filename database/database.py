775af8cf2d7e5c1da2e58e15d81b298928b1f95f
Corey Barbera <160194877+Dismodified@users.noreply.github.com>
2025-09-06 12:30:42 -0400

    Update database.py

--- database/database.py
+++ database/database.py
@@ -11,19 +11,20 @@ INSERT INTO Users (FirstName, LastName, Login, Password) VALUES
     ('Corey','Corey','Corey','Corey'),
     ('Sam','Hill','SamH','Test');
 
-INSERT INTO Contacts (FirstName, LastName, Phone, Email, Address, UserID) VALUES
-('Corey','Corey','123-456-7890','sample@ucf.edu','012 Street, City, State',1),
-('Corey','Corey','123-456-7890','sample@ucf.edu','012 Street, City, State',2),
-('Corey','Corey','123-456-7890','sample@ucf.edu','012 Street, City, State',3),
-('Sam','Hill','987-654-3210','test@ucf.edu','345 Street, City, State',1),
-('Sam','Hill','987-654-3210','test@ucf.edu','345 Street, City, State',2),
-('Sam','Hill','987-654-3210','test@ucf.edu','345 Street, City, State',3),
-('UniqueTo','Rick','111-222-3333','1@ucf.edu','0 Ave, City, State',1),
-('UniqueTo','Admin','444-555-6666','2@ucf.edu','1 Ave, City, State',2),
-('UniqueTo','Test','777-888-9999','3@ucf.edu','3 Street, City, State',3);
+INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID) VALUES
+('Corey','Corey','123-456-7890','sample@ucf.edu',1),
+('Corey','Corey','123-456-7890','sample@ucf.edu',2),
+('Corey','Corey','123-456-7890','sample@ucf.edu',3),
+('Sam','Hill','987-654-3210','test@ucf.edu',1),
+('Sam','Hill','987-654-3210','test@ucf.edu',2),
+('Sam','Hill','987-654-3210','test@ucf.edu',3),
+('UniqueTo','Rick','111-222-3333','1@ucf.edu',1),
+('UniqueTo','Admin','444-555-6666','2@ucf.edu',2),
+('UniqueTo','Test','777-888-9999','3@ucf.edu',3);
 
 SHOW TABLES;
 SELECT COUNT(*) FROM Users;
 SELECT COUNT(*) FROM Contacts;
+
 
 
